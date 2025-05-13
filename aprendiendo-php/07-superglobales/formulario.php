25df526ba6b4a550e8e86f6ff18b05660447504a
F7Epsum <fede.carassale@gmail.com>
2025-05-13 17:41:57 +0200

    variables superglobales -get

---
+++ aprendiendo-php/07-superglobales/formulario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Formulario en PHP</title>
+    </head>
+    <body>
+        <h1>Formulario PHP</h1>
+
+        <form method="get" action="recibir.php">
+            <p>
+                <label for="nombre">Nombre</label>
+                <input type="text" name="nombre"/>
+            </p>
+
+            <p>
+                <label for="apellidos">Nombre</label>
+                <input type="text" name="apellidos"/>
+            </p>
+
+            <input type="submit" value="Enviar datos"/>
+        </form>
+    </body>
+</html>
+
+<?php
+
--- aprendiendo-php/07-superglobales/index.php
+++ aprendiendo-php/07-superglobales/index.php
@@ -11,4 +11,6 @@
 
     //Variables GET
 
+    
+
 ?>
