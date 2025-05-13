c18dc0de0e1206f8a55789ee6a011b1569b04919
F7Epsum <fede.carassale@gmail.com>
2025-05-13 11:32:30 +0200

    aprendiendo operadores

--- aprendiendo-php/06-operadores/index.php
+++ aprendiendo-php/06-operadores/index.php
@@ -7,7 +7,14 @@
     echo "Suma: " . ($num1+$num2) . '<br/>';
     echo "Resta: " . ($num1-$num2) . '<br/>';
     echo "Multiplicacion: " . ($num1*$num2) . '<br/>';
+    echo "Division: " . ($num1/$num2) . '<br/>';
+    echo "Modulo-Resto: " . ($num1%$num2) . '<br/>';
 
+    //Operadores incremento y decremento
+    $year = 2019;
+    $year++;
+
+    echo "<h1>$year</h1>"
 
 
 
