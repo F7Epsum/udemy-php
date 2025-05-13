40228110968646c126e2330e3e49a095c683a206
F7Epsum <fede.carassale@gmail.com>
2025-05-13 10:42:02 +0200

    aprendiendo php operadores

---
+++ aprendiendo-php/06-operadores/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    //Operadores aritmeticos
+    $num1 = 55;
+    $num2 = 33;
+
+    echo "Suma: " . ($num1+$num2) . '<br/>';
+    echo "Resta: " . ($num1-$num2) . '<br/>';
+    echo "Multiplicacion: " . ($num1*$num2) . '<br/>';
+
+
+
+
+?>
