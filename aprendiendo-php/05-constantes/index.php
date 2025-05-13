66ff88bb27e2b010e6def9f0934258857d37f3a4
F7Epsum <fede.carassale@gmail.com>
2025-05-13 10:32:34 +0200

    aprendiendo constantes y constantes predefinidas

---
+++ aprendiendo-php/05-constantes/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    define('nombre', 'Feca 7');
+    define('web', 'feca7.com');
+
+    echo '<h1>' . nombre . '</h1>';
+    echo '<h1>' . web . '</h1>';
+
+    //Constantes predefinidas
+
+    echo PHP_VERSION;
+    echo PHP_OS;
+    echo PHP_EXTENSION_DIR;
+    echo __LINE__;
+    echo __FILE__;
+    echo __FUNCTION__;
+    
+
+
+
+
+?>
