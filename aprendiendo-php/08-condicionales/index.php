47cfa455348d474db51fcc4a2e06d1fa64b35bd1
F7Epsum <fede.carassale@gmail.com>
2025-05-14 13:09:13 +0200

    condicionales PHP

---
+++ aprendiendo-php/08-condicionales/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    //Condicionales if
+
+    // Operadores de comparador
+    // == igual
+    // === identico
+    // != distinto
+    // <> diferente
+    // !== no identico
+
+    $color = "verde";
+
+    if($color == "rojo"){
+        echo "El color es rojo";
+    } else{
+        echo "el color NO es rojo";
+    };
+
+
+?>
